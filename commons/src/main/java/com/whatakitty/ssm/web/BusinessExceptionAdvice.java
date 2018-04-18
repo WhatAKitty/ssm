e09cd5f9599ee3b97b87da810eb570f46e51548b
WhatAKitty <104xuqiang@163.com>
2018-04-18 23:45:29 +0800

    Convert error html view to json response

--- commons/src/main/java/com/whatakitty/ssm/web/BusinessExceptionAdvice.java
+++ commons/src/main/java/com/whatakitty/ssm/web/BusinessExceptionAdvice.java
@@ -1,11 +1,17 @@
 package com.whatakitty.ssm.web;
 
+import com.alibaba.fastjson.JSON;
 import com.whatakitty.ssm.exception.BusinessException;
 import com.whatakitty.ssm.response.BodyBuilder;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,12 +20,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @date 2018/04/09
  * @description
  **/
+@Log4j2
 @ControllerAdvice
-public class BusinessExceptionAdvice {
+public class BusinessExceptionAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity businessException(final BusinessException e) {
         return new ResponseEntity(BodyBuilder.create().addError(e.getMessage()).build(), HttpStatus.valueOf(e.getCode()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (logger.isErrorEnabled()) {
+            logger.error(ex.getMessage(), ex);
+        }
+
+        return new ResponseEntity(BodyBuilder.create().addError(ex.getMessage()).build(), status);
     }
 
 }
